e649275c5f0357df43e0d65cdca1dce7d6b3e702
EmirBuric <160528544+EmirBuric@users.noreply.github.com>
2025-02-08 21:17:10 +0100

    Korisnik input
    Koorisnik preko inputa dodaje kucu

--- SkylinesSmartHouseConsoleApp/Program.cs
+++ SkylinesSmartHouseConsoleApp/Program.cs
@@ -22,6 +22,27 @@ public class Program
 
         var kucaServis = scope.ServiceProvider.GetRequiredService<KucaService>();
 
+        var dodajKucu = new KucaDodaj();
+
+        Console.WriteLine("Zdravo, unesite naziv kuce: ");
+        dodajKucu.Naziv = Console.ReadLine();
+        while (string.IsNullOrEmpty(dodajKucu.Naziv)) 
+        {
+            Console.WriteLine("Molimo Vas da unesete naziv");
+            dodajKucu.Naziv = Console.ReadLine();
+        }
+        Console.WriteLine("Unesite povsinu u m^2");
+        string unosDodaj= Console.ReadLine();
+        int tempDodajPov;
+        while (!Int32.TryParse(unosDodaj, out tempDodajPov))
+        {
+            Console.WriteLine("Molimo Vas da unesete povrsinu");
+            unosDodaj = Console.ReadLine();
+        }
+        dodajKucu.Povrsina = tempDodajPov;
+
+        kucaServis.Insert(dodajKucu);
+
         kucaServis.Insert(new KucaDodaj
         {
             Naziv = "Moja Kuca",
@@ -34,18 +55,57 @@ public class Program
             Povrsina = 124
         });
 
-        var kuca = kucaServis.GetById(1);
+        var pretraga= new KucaPretraga();
+        Console.WriteLine("Pretrazite kuce po nazivu");
+        pretraga.Naziv = Console.ReadLine();
+        while (string.IsNullOrEmpty(pretraga.Naziv))
+        {
+            Console.WriteLine("Molimo Vas da unesete naziv");
+            pretraga.Naziv = Console.ReadLine();
+        }
 
-        Console.WriteLine($"Kuća dodata,getById: {kuca.Naziv}, {kuca.Povrsina} m^2");
 
-        var kuce = kucaServis.GetSearchRes(new KucaPretraga { Naziv = "Moja Kuca 1" });
+        var kuce = kucaServis.GetSearchRes(pretraga);
 
         foreach (Kuca kuca1 in kuce)
         {
-            Console.WriteLine($"Kuća dodata,GetSearch: {kuca1.Naziv}, {kuca1.Povrsina} m^2");
-        }     
+            Console.WriteLine($"Kuća dodata,GetSearch: {kuca1.Naziv}, {kuca1.Povrsina} m^2, Id:{kuca1.Id}");
+        }
 
-        kucaServis.Update(1, new KucaAzuriraj { Naziv = "Moja kuca update", Povrsina =134 });
+        var update = new KucaAzuriraj();
+
+        Console.WriteLine("Azuriranje kuce");
+        Console.WriteLine("Unesite Id kuce koju zelite azurirat");
+        string unosId= Console.ReadLine();
+        int tempId;
+        while (!Int32.TryParse(unosId, out tempId))
+        {
+            Console.WriteLine("Molimo Vas da unesete Id");
+            unosId = Console.ReadLine();
+        }
+        int id = tempId;
+
+        Console.WriteLine("Unesite novi naziv kuce: ");
+        update.Naziv = Console.ReadLine();
+        while (string.IsNullOrEmpty(update.Naziv))
+        {
+            Console.WriteLine("Molimo Vas da unesete naziv");
+            update.Naziv = Console.ReadLine();
+        }
+        int tempUpdatePov;
+        Console.WriteLine("Unesite povsinu u m^2");
+        string unosUpdatePov = Console.ReadLine();
+        while (!Int32.TryParse(unosUpdatePov, out tempUpdatePov))
+        {
+            Console.WriteLine("Molimo Vas da unesete povrsinu");
+            unosUpdatePov = Console.ReadLine();
+        }
+        update.Povrsina = tempUpdatePov;
+
+
+        kucaServis.Update(tempId, update);
+
+        var kuca= kucaServis.GetById(tempId);
 
         Console.WriteLine($"Kuća update,getById: {kuca.Naziv}, {kuca.Povrsina} m^2");
     }
--- SkylinesSmartHouseConsoleApp/Servisi/KucaService.cs
+++ SkylinesSmartHouseConsoleApp/Servisi/KucaService.cs
@@ -2,11 +2,6 @@
 using SkylinesSmartHouseConsoleApp.Dodaj;
 using SkylinesSmartHouseConsoleApp.Modeli;
 using SkylinesSmartHouseConsoleApp.Pretrazi;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SkylinesSmartHouseConsoleApp.Servisi
 {
